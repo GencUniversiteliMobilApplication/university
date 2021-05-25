9fbef999c6e0cfb436541754f10c30ef3561489e
Zeynepizmm23 <mobilapplication23@gmail.com>
2021-05-25 12:50:14 +0300

    CanlıderstenÖncekiYazdığımızKodlar

---
+++ app/src/main/java/com/example/university/HukukActivity.java
@@ -0,0 +1,14 @@
+package com.example.university;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HukukActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hukuk);
+    }
+}
---
+++ app/src/main/java/com/example/university/VeterinerlikSorActivity.java
@@ -0,0 +1,14 @@
+package com.example.university;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VeterinerlikSorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veterinerlik_sor);
+    }
+}
